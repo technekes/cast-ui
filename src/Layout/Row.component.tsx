c62e3e2cf8e76500dd3300e47a9135493fd5ffa6
Dantiff <20873103+Dantiff@users.noreply.github.com>
2019-01-29 12:21:03 +0300

    initial setup for the Row component

--- src/Layout/Row.component.tsx
+++ src/Layout/Row.component.tsx
@@ -3,12 +3,6 @@ import styled from 'styled-components';
 
 type Props = {
   /**
-   * Allows the container element to always occupy 100% with of the parent element
-   *
-   * @default false
-   **/
-  fluid: boolean;
-  /**
    * From theme provider
    *
    * @default defaultTheme
@@ -16,49 +10,35 @@ type Props = {
   theme?: any;
 };
 
-const SContainer = styled.div`
+const SRow = styled.div`
   width: 100%;
   height: auto;
-  margin-right: auto;
-  margin-left: auto;
+  // margin-right: auto;
+  // margin-left: auto;
   padding-right: ${(props: Props) => props.theme.layout.gutters.xs};
   padding-left: ${(props: Props) => props.theme.layout.gutters.xs};
 
   @media (min-width: ${(props: Props) => props.theme.layout.breakpoints.sm}) {
-    max-width: ${(props: Props) =>
-      `${props.fluid ? '100%' : props.theme.layout.fluidWidths.sm}`};
     padding-right: ${(props: Props) => props.theme.layout.gutters.sm};
     padding-left: ${(props: Props) => props.theme.layout.gutters.sm};
   }
 
   @media (min-width: ${(props: Props) => props.theme.layout.breakpoints.md}) {
-    max-width: ${(props: Props) =>
-      `${props.fluid ? '100%' : props.theme.layout.fluidWidths.md}`};
     padding-right: ${(props: Props) => props.theme.layout.gutters.md};
     padding-left: ${(props: Props) => props.theme.layout.gutters.md};
   }
 
   @media (min-width: ${(props: Props) => props.theme.layout.breakpoints.lg}) {
-    max-width: ${(props: Props) =>
-      `${props.fluid ? '100%' : props.theme.layout.fluidWidths.lg}`};
     padding-right: ${(props: Props) => props.theme.layout.gutters.lg};
     padding-left: ${(props: Props) => props.theme.layout.gutters.lg};
   }
 
   @media (min-width: ${(props: Props) => props.theme.layout.breakpoints.xl}) {
-    max-width: ${(props: Props) =>
-      `${props.fluid ? '100%' : props.theme.layout.fluidWidths.xl}`};
     padding-right: ${(props: Props) => props.theme.layout.gutters.xl};
     padding-left: ${(props: Props) => props.theme.layout.gutters.xl};
   }
 `;
 
-export const Container: React.FunctionComponent<Props> = ({
-  fluid = false,
-  children,
-  theme,
-}) => (
-  <SContainer fluid={fluid} theme={theme}>
-    {children}
-  </SContainer>
+export const Row: React.FunctionComponent<Props> = ({ children, theme }) => (
+  <SRow theme={theme}>{children}</SRow>
 );
--- src/Layout/index.ts
+++ src/Layout/index.ts
@@ -1,3 +1,4 @@
 import { Container } from './Container.component';
+import { Row } from './Row.component';
 
-export { Container };
+export { Container, Row };
