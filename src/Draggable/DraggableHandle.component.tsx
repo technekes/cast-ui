fc9cbd6b5a837f343ce91ebf92df582f4086faf0
Dantiff <20873103+Dantiff@users.noreply.github.com>
2019-02-15 10:20:47 +0300

    upgrade but build fails

--- src/Draggable/DraggableHandle.component.tsx
+++ src/Draggable/DraggableHandle.component.tsx
@@ -1,28 +1,44 @@
-import * as React from 'react';
-import Icon from 'react-icons-kit';
-import { ic_view_headline as icViewHeadline } from 'react-icons-kit/md/ic_view_headline';
+// import * as React from 'react';
+// import Icon from 'react-icons-kit';
+// import { ic_view_headline as icViewHeadline } from 'react-icons-kit/md/ic_view_headline';
 
-type Props = {
-  /**
-   * Pass in Handle Icon iconsize
-   *
-   * @default '40'
-   **/
-  size?: number;
-  /**
-   * From theme provider
-   *
-   * @default defaultTheme
-   **/
-  theme?: any;
-};
+// type Props = {
+//   /** Activae Hover handle  */
+//   onMouseEnter?: Function;
+//   /** Deactivate Hover handle  */
+//   onMouseLeave?: Function;
+//   /**
+//    * Pass in Handle Icon iconsize
+//    *
+//    * @default '40'
+//    **/
+//   size?: number;
+//   /**
+//    * Pass in Handle css className
+//    *
+//    * @default 'handleIcon'
+//    **/
+//   className?: string;
+//   /**
+//    * From theme provider
+//    *
+//    * @default defaultTheme
+//    **/
+//   theme?: any;
+// };
 
-export const DraggableHandle: React.FunctionComponent<Props> = props => (
-  <React.Fragment>
-    <Icon icon={icViewHeadline} {...props} className="handleIcon" />
-    {props.children}
-  </React.Fragment>
-);
-DraggableHandle.defaultProps = {
-  size: 25,
-};
+// export const DraggableHandle: React.FunctionComponent<Props> = props => (
+//   <div
+//     onMouseEnter={() => props.onMouseEnter}
+//     onMouseLeave={() => props.onMouseLeave}>
+//     <Icon
+//       icon={icViewHeadline}
+//       {...props}
+//       className={`handleIcon ${props.className}`}
+//     />
+//     {props.children}
+//   </div>
+// );
+// DraggableHandle.defaultProps = {
+//   size: 25,
+// };
